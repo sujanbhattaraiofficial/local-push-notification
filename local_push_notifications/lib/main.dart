11d8f7bc15879be95f22df4d8763f5e80332373f
sujan bhattarai <sujanb681@gmail.com>
2020-03-27 09:46:33 +0545

    initial commit

---
+++ local_push_notifications/lib/main.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LocalNotifications(),
+    );
+  }
+}
+
+class LocalNotifications extends StatefulWidget {
+  @override
+  _LocalNotificationsState createState() => _LocalNotificationsState();
+}
+
+class _LocalNotificationsState extends State<LocalNotifications> {
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  AndroidInitializationSettings androidInitializationSettings;
+  IOSInitializationSettings iosInitializationSettings;
+  InitializationSettings initializationSettings;
+
+  @override
+  void initState() {
+    super.initState();
+    ok();
+  }
+
+  void _showNotification() async {
+    await notificationSchedule();
+  }
+
+  void ok() async {
+    androidInitializationSettings = AndroidInitializationSettings('app_icon');
+    iosInitializationSettings = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initializationSettings = InitializationSettings(
+        androidInitializationSettings, iosInitializationSettings);
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
+  }
+
+  Future<void> notificationSchedule() async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your other channel id',
+        'your other channel name',
+        'your other channel description');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    NotificationDetails platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0,
+        'scheduled title',
+        'scheduled body',
+        // scheduledNotificationDateTime,
+        platformChannelSpecifics);
+  }
+
+  Future onSelectNotification(String payLoad) {
+    if (payLoad != null) {
+      debugPrint("$payLoad");
+    }
+    print("Notification Tab");
+// If we tab in the notification we can set the nagivator to navigate to the next page,
+  }
+
+  Future onDidReceiveLocalNotification(
+      int id, String title, String body, String paload) async {
+    return showDialog(
+        context: context,
+        builder: (
+          BuildContext context,
+        ) =>
+            CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(body),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text("Okay"),
+                  onPressed: () {
+                    // naviagate to desire page
+                  },
+                )
+              ],
+            ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(onPressed: _showNotification),
+    );
+  }
+}
